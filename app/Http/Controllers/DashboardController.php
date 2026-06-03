8bfe7b7d7ecca30a70546dd155fd68b964093085
runidwii <runidwiwe@gmail.com>
2026-06-03 16:55:20 +0700

    update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -14,7 +14,7 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        $totalObat = Obat::count();
+        $totalObat = Persediaan::sum('stok_terkini');
 
         $stokMenipis = Persediaan::whereColumn(
             'stok_terkini',
